70e93bc281ec1998206d756f5f026fba677297ff
gabriel marques <gabrielmarksf@icloud.com>
2024-11-30 20:23:29 -0300

    feat: Corrigindo parâmetro recebido no construtor

--- src/main/java/com/construcontrol/construcontrol/model/domain/users/User.java
+++ src/main/java/com/construcontrol/construcontrol/model/domain/users/User.java
@@ -18,67 +18,75 @@ import java.util.List;
 @Table(name = "users")
 @ToString
 public class User implements UserDetails {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-  @Column(name = "name")
-  protected String name;
-  @Column(name = "cpf", unique = true, length = 11)
-  protected String cpf;
-  @Column(name = "rg", unique = true, length = 9)
-  protected String rg;
-  @Column(name = "phone")
-  protected String phone;
-  @Column(unique = true, name = "email")
-  protected String email;
-  @Enumerated(EnumType.STRING)
-  UserRole userRole;
-  @Column(name = "password")
-  protected String password;
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "address_id", referencedColumnName = "id")
-  protected Address address;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-  public User(String login, String password, UserRole userRole) {
-    this.email = login;
-    this.password = password;
-    this.userRole = userRole;
-  }
+    @Column(name = "name")
+    protected String name;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    if (this.userRole == UserRole.GESTOR)
-      return List.of(new SimpleGrantedAuthority("ROLE_GESTOR"), new SimpleGrantedAuthority("ROLE_CLIENT"));
-    else return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
-  }
+    @Column(name = "cpf", unique = true, length = 11)
+    protected String cpf;
 
-  @Override
-  public String getPassword() {
-    return password;
-  }
+    @Column(name = "rg", unique = true, length = 9)
+    protected String rg;
 
-  @Override
-  public String getUsername() {
-    return email;
-  }
+    @Column(name = "phone")
+    protected String phone;
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    @Column(unique = true, name = "email")
+    protected String email;
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    @Enumerated(EnumType.STRING)
+    UserRole userRole;
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Column(name = "password")
+    protected String password;
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    protected Address address;
+
+    public User(String email, String password, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.userRole == UserRole.GESTOR)
+            return List.of(new SimpleGrantedAuthority("ROLE_GESTOR"), new SimpleGrantedAuthority("ROLE_CLIENT"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
