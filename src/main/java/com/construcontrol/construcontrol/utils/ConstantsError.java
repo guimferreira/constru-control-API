48d0270831c38cbb7027043a55ee2586b9d0e637
gabriel marques <gabrielmarksf@icloud.com>
2024-12-02 20:37:21 -0300

    fix: Removendo constantes não utilizadas

--- src/main/java/com/construcontrol/construcontrol/utils/ConstantsError.java
+++ src/main/java/com/construcontrol/construcontrol/utils/ConstantsError.java
@@ -32,13 +32,11 @@ public final class ConstantsError {
     public static final String MESSAGE_END_DATE_BLANK = "Data de término não pode ser vazia";
     public static final int USERNAME_MIN_LENGTH = 2;
     public static final int USERNAME_MAX_LENGTH = 50;
-    public static final int CPF_LENGTH = 11;
     public static final int PASSWORD_MIN_LENGTH = 8;
     public static final int PHONE_MIN_LENGTH = 11;
     public static final int RG_MIN_LENGTH = 5;
     public static final int RG_MAX_LENGTH = 15;
     public static final int CEP_LENGTH = 8;
-    public static final int CNPJ_LENGTH = 14;
 
     private ConstantsError() {
         throw new UnsupportedOperationException("Essa classe não pode ser instanciada");
