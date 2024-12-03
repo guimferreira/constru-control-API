0e629b292fdfdec5869219c9f41b4c720ca61696
gabriel marques <gabrielmarksf@icloud.com>
2024-12-02 21:09:04 -0300

    fix: Alterando notação bean

--- src/main/java/com/construcontrol/construcontrol/DTO/projects/ApartamentDTO.java
+++ src/main/java/com/construcontrol/construcontrol/DTO/projects/ApartamentDTO.java
@@ -1,7 +1,6 @@
 package com.construcontrol.construcontrol.DTO.projects;
 
 import com.construcontrol.construcontrol.utils.ConstantsError;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,17 +12,16 @@ import lombok.NoArgsConstructor;
 public class ApartamentDTO {
     private long id;
 
-    @NotBlank(message = ConstantsError.MESSAGE_NAME_CONSTRUCTION_BLANK)
+    @NotNull(message = ConstantsError.MESSAGE_NAME_CONSTRUCTION_BLANK)
     private ConstructionDTO construction;
 
-    @NotBlank(message = ConstantsError.MESSAGE_NUMBER_APARTMENT_BLANK)
+    @NotNull(message = ConstantsError.MESSAGE_NUMBER_APARTMENT_BLANK)
     private String number;
 
-
-    @NotBlank(message = ConstantsError.MESSAGE_AREA_APARTMENT_BLANK)
+    @NotNull(message = ConstantsError.MESSAGE_AREA_APARTMENT_BLANK)
     private double area;
 
-    @NotBlank(message = ConstantsError.MESSAGE_PRICE_APARTMENT_BLANK)
+    @NotNull(message = ConstantsError.MESSAGE_PRICE_APARTMENT_BLANK)
     private double price;
 
     @NotNull(message = ConstantsError.MESSAGE_SOLD_STATUS_APARTMENT_BLANK)
